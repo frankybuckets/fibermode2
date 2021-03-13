8d19d40c9eaf16c49badd6ca63f1865703cbdcd9
bquanahparker <bqp2@pdx.edu>
2021-03-12 19:50:55 -0800

    changed radii to pick up LP-like modes

--- fiber/microstruct/arf_poletti_mode.py
+++ fiber/microstruct/arf_poletti_mode.py
@@ -6,15 +6,14 @@ from arf import ARF
 
 a = ARF(name='poletti', freecapil=False)
 p = 3         # finite element degree
-# a.refine()
-
+a.refine()
 
 # compute:
 Zs, Ys, Yls, betas, P, _, _, _ =  \
     a.polyeig(p=p,
               #    LP01,   LP11,  LP21   LP02
               ctrs=(2.24,  3.57,  4.75,  5.09),
-              radi=(0.01,  0.01,  0.01,  0.01),
+              radi=(0.02,  0.02,  0.02,  0.02),
               npts=4, alpha=5,
               eta_tol=1.e-12, stop_tol=1e-12)
 print('All computes eigenvalues:\n')
@@ -25,5 +24,12 @@ print(' betas =', betas)
 
 Ys[-1].draw()  # visualize in netgen window
 
-# a.savemodes('tmp_arfLP01_p%d' % p, Ys[0], p, betas, Zs,
-#             {'method': 'polyeig', 'Zs': Zs, 'name': 'LP01'}, arfpickle=True)
+# save the modes to file
+modenames = ['LP01', 'LP11', 'LP21', 'LP02']
+fileprefixes = ['tmp_arf{0}_p{1}'.format(name, p) for name in modenames]
+
+for k in range(len(fileprefixes)):
+    solverparams = {'method': 'polyeig', 'Zs': (Zs[k],),
+                    'name': modenames[k]}
+    a.savemodes(fileprefixes[k], Ys[k], p, (betas[k],), (Zs[k],),
+                solverparams, arfpickle=True)
