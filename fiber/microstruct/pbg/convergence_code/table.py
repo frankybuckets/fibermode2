102c720b6574fe398ff476d540fc2c8f3a92a90b
pieter <pumpkinpieter@gmail.com>
2021-05-23 16:11:37 -0700

    Updated table.py in convergence_code.

--- fiber/microstruct/pbg/convergence_code/table.py
+++ fiber/microstruct/pbg/convergence_code/table.py
@@ -5,22 +5,14 @@ Created on Sat May 22 11:10:31 2021.
 
 @author: pv
 """
-import netgen.gui
-from fiberamp.fiber.microstruct.pbg.fiber_dicts.lyr6cr2 import params
-from fiberamp.fiber.microstruct.pbg import PBG
 import numpy as np
 import os
-import ngsolve as ng
 from prettytable import PrettyTable
 
 # %%
 
-table = PrettyTable(padding_width=5)
-headers = ['Degree', 'DoFs', 'Confinement Loss (dB/m)']
-# table.field_names = headers
-
 ps = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-refs = [0]
+refs = [1, 2]
 
 folder = '/home/pv/local/fiberamp/fiber/microstruct/pbg/\
 outputs/lyr6cr2/convergence_studies'
@@ -30,43 +22,38 @@ if not os.path.isdir(os.path.relpath(folder)):
     raise ValueError("Given folder is not a directory.  Make this directory \
 and begin again.")
 
-all_data = []
+D = {}
 
 for ref in refs:
-    data = []
+    ref_D = {}
+    CL = []
+    ndofs = []
+
     for p in ps:
-        filename = 'p' + str(p) + '_refs' + str(ref) + '.npz'
-        filepath = os.path.abspath(folder + '/' + filename)
-        d = np.load(filepath)
-        data.append(d['CL'][0])
-    all_data.append(data)
+        try:
+            filename = 'p' + str(p) + '_refs' + str(ref) + '.npz'
+            filepath = os.path.abspath(folder + '/' + filename)
+            d = np.load(filepath)
+            CL.append(d['CL'][0])
+            ndofs.append(d['ndofs'])
 
-table.add_column(headers[0], ps)
-table.add_column(headers[2], all_data[0])
+        except FileNotFoundError:
+            CL.append('--')
+            ndofs.append('--')
 
-
-# %%
-center = 1.242933-2.471929e-09j
-radius = .01
-p = 2
-
-A = PBG(params)
-ng.Draw(A.mesh)
-
+    ref_D['CL'] = CL
+    ref_D['ndofs'] = ndofs
+    D[ref] = ref_D
 
 # %%
+r = 1
 
-print(A.check_ndof(3, 3))
-ng.Draw(A.mesh)
+table = PrettyTable(title='Refinements: ' + str(r), padding_width=5)
+fields = ['Refinements', 'Degree', 'Confinement Loss (dB/m)', 'DoFs']
 
-# %%
+table.add_column(fields[1], ps)
+table.add_column(fields[2], D[r]['CL'])
+table.add_column(fields[3], D[r]['ndofs'])
 
-dofs = []
-for p in ps:
-    _, X = A.polypmlsystem(p=p, alpha=A.alpha)
-    dofs.append(X.ndof)
 
-# %%
-
-table.add_column(headers[1], dofs)
 print(table)
