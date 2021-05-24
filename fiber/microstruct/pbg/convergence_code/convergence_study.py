6d6a8f38833a1815b2539049eb46d2f9a3767439
pieter <pumpkinpieter@gmail.com>
2021-05-23 18:42:57 -0700

    Convergence code updates.

--- fiber/microstruct/pbg/convergence_code/convergence_study.py
+++ fiber/microstruct/pbg/convergence_code/convergence_study.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 21 14:26:16 2021.
-
-@author: pv
-
-A general convergence study code for our fibers.
 
 """
 from fiberamp.fiber.microstruct.pbg import PBG
@@ -13,54 +8,85 @@ from fiberamp.fiber.microstruct.pbg.fiber_dicts.lyr6cr2 import params
 import numpy as np
 import os
 
+"""
+General convergence study code. To use:
 
-def modefind(fiber_obj, center, radius, p, ref, nspan=2, npts=4):
-    """Compute modes of fiber object."""
-    for i in range(ref):
-        fiber_obj.refine()
+    1: Enter desired fiber name and mode you wish to study. Make sure the
+    correct center is listed.  Change import of parameters (above) to reflect
+    fiber name.
 
-    z, y, _, beta, P, _ = fiber_obj.leakymode(p, rad=radius, ctr=center,
-                                              alpha=fiber_obj.alpha,
-                                              niterations=20, npts=npts,
-                                              nspan=nspan, nrestarts=1)
-    return z, y, beta, P.fes.ndof
+    2: Enter your path to the pbg folder in fiberamp and create the output
+    directory <pbg_home>/outputs/<fiber_name>/<mode_name>/convergence.  Or
+    create directory of your choice and set it to variable <folder>.
+
+    3: Alter polynomial degree list (ps) and refinement list (refs) to desired
+    values. For our study, ps = [2,3,4,5,6,7,8,9,10,11] and refs = [0,1,2,3].
+    This file is saved with ps = [1], refs = [0] for debugging purposes.
+    Memory allocation errors are handled by passing to next iterate.
+
+    4: Alter radius, nspan and npts as desired.  Center is set by mode name
+    and shouldn't be changed.
+
+After saving, alter script.h file appropriately and run sbatch script.h.
+"""
+
+# Fiber and mode names on which to perform convergence study. ##
+fiber_name = 'lyr6cr2'   # Note: change import above to correspond.
+mode_name = 'LP01'
+
+
+# Starting location for eigenvalue iterations. #########################
+centers = {'LP01': 1.242933-2.471929e-09j,
+           'LP11': 1.93487063-8.699515e-08j}
+
+
+# Folder setup.  Enter your path to pbg folder. ##################
+pbg_home = '/home/pv/local/fiberamp/fiber/microstruct/pbg/'
+folder = pbg_home + '/outputs/' + fiber_name + \
+    '/' + mode_name + '/' + 'convergence'   # Make this directory
+
+if not os.path.isdir(os.path.relpath(folder)):
+    raise FileNotFoundError("Given folder is not a directory. Make this \
+                            directory and begin again.")
+
+# Polynomial Degrees and Refinements to cycle through. #################
+ps = [1]
+refs = [0]
+
+
+# Center, radius and span size for FEAST. ##############################
+center = centers[mode_name]
+radius = .001
+nspan = 2
+npts = 4                                  # number of quadrature points
 
 
 if __name__ == '__main__':
 
-    ps = [2, 3, 4, 5]         # Polynomial degrees to cycle through
-    refs = [3]                # Refinements to cycle through
+    for p in ps:
+        for ref in refs:
+            print('Polynomial degree %i, with %i refinements: ' % (p, ref))
+            print('Building fiber object and refining.\n')
 
-    center = 1.242933-2.471929e-09j    # Center for FEAST
-    radius = .01                       # Radius for FEAST
-    nspan = 2                          # Number of vectors for initial span
+            A = PBG(params)
+            for i in range(ref):
+                A.refine()
 
-    folder = '/home/pv/local/fiberamp/fiber/microstruct/pbg/outputs/lyr6cr2/\
-fund_mode/convergence'               # Folder for outputs
-
-    if not os.path.isdir(os.path.relpath(folder)):
-
-        raise ValueError(
-            "Given folder is not a directory.  Make this directory and begin\
- again.")
-
-    else:
-        for p in ps:
-            for ref in refs:
-                print('Polynomial degree %i, with %i refinements: ' % (p, ref))
-                print('Building fiber object.\n')
-                A = PBG(params)
-
-                print('Refining mesh and finding modes.\n')
-                z, _, beta, ndof = modefind(A, center, radius, p, ref,
-                                            nspan=nspan)
-
-                print("Found modes.\n")
+            print('Finding modes.\n')
+            try:
+                z, y, _, beta, P, _ = A.leakymode(p, rad=radius, ctr=center,
+                                                  alpha=A.alpha,
+                                                  niterations=20, npts=npts,
+                                                  nspan=nspan, nrestarts=1)
+                ndof = P.fes.ndof
                 CL = 20 * beta.imag / np.log(10)
+                d = {'z': z, 'beta': beta, 'p': p,
+                     'ref': ref, 'CL': CL, 'ndofs': ndof}
+
+                print('Saving data.\n')
                 filename = 'p' + str(p) + '_refs' + str(ref)
                 filepath = os.path.abspath(folder + '/' + filename)
-
-                d = {'z': z, 'beta': beta,
-                     'p': p, 'ref': ref, 'CL': CL, 'ndofs': ndof}
-                print('Saving data.\n')
                 np.savez(filepath, **d)
+
+            except MemoryError("Unable to find modes due to MemoryError."):
+                pass
--- fiber/microstruct/pbg/convergence_code/table.py
+++ fiber/microstruct/pbg/convergence_code/table.py
@@ -12,10 +12,10 @@ from prettytable import PrettyTable
 # %%
 
 ps = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-refs = [1, 2]
+refs = [0, 1, 2]
 
 folder = '/home/pv/local/fiberamp/fiber/microstruct/pbg/\
-outputs/lyr6cr2/convergence_studies'
+outputs/lyr6cr2/LP01/convergence'
 
 if not os.path.isdir(os.path.relpath(folder)):
 
@@ -35,25 +35,25 @@ for ref in refs:
             filepath = os.path.abspath(folder + '/' + filename)
             d = np.load(filepath)
             CL.append(d['CL'][0])
-            ndofs.append(d['ndofs'])
+            # ndofs.append(d['ndofs'])
 
         except FileNotFoundError:
             CL.append('--')
             ndofs.append('--')
 
     ref_D['CL'] = CL
-    ref_D['ndofs'] = ndofs
+    # ref_D['ndofs'] = ndofs
     D[ref] = ref_D
 
 # %%
-r = 1
+r = 0
 
 table = PrettyTable(title='Refinements: ' + str(r), padding_width=5)
 fields = ['Refinements', 'Degree', 'Confinement Loss (dB/m)', 'DoFs']
 
 table.add_column(fields[1], ps)
 table.add_column(fields[2], D[r]['CL'])
-table.add_column(fields[3], D[r]['ndofs'])
+# table.add_column(fields[3], D[r]['ndofs'])
 
 
 print(table)
